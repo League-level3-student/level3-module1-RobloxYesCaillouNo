922c051f78be56ef6adf5f3532e582bf2a2aa677
RobloxYesCaillouNo <jaydenkudo@gmail.com>
2019-06-16 16:27:45 -0700

    af

--- src/_03_IntroToStacks/_01_IntroToStack.java
+++ src/_03_IntroToStacks/_01_IntroToStack.java
@@ -12,16 +12,20 @@ public class _01_IntroToStack {
 		Stack<Double> doubles = new Stack<Double>();
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
 		Random rand = new Random();
+		double range = 100 - 0;
 		
-		for (int i = 0; i < args.length; i++) {
-	
+		for (int i = 0; i < doubles.size(); i++) {
+			doubles.push(range);
+			rand.nextDouble();
+			
 		}
 		
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-		
+		JOptionPane.showInputDialog("enter 2 numbers between 0 and 100");
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
-		
+		System.out.print("NUM 1: " + doubles.pop());
+		System.out.print("NUM 2: "+ doubles.pop());
 		
 		//   EXAMPLE:
 		//   NUM 1: 65
