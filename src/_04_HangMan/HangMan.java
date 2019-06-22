b7a7126ca269a54a5e08562d9a83d9886fac8277
RobloxYesCaillouNo <jaydenkudo@gmail.com>
2019-06-21 18:23:59 -0700

    some stuff

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,23 @@
 package _04_HangMan;
 
+import java.util.Stack;
+
+import javax.swing.JOptionPane;
+
 public class HangMan{
 	
+	public static void main(String[] args) {
+	Stack<Utilities> words = new Stack<Utilities>();
+		
+		
+		String input = JOptionPane.showInputDialog("How many words would you like to guess up to 266??");
+		int inputt = Integer.parseInt(input);
+		
+	Utilities.readRandomLineFromFile("dictionary.txt");
+	if (words.contains(input)) {
+		
+	}
 	
+	}
 	
 }
