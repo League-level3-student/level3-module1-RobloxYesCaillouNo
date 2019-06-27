d3757145fc6a36fd854a584c0e87594097a6b7be
RobloxYesCaillouNo <jaydenkudo@gmail.com>
2019-06-26 18:37:33 -0700

    hashmaps

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -11,23 +11,28 @@ import javax.swing.JPanel;
 
 public class HangMan implements KeyListener {
 	static String word;
-	static String line;
+	static String line = "";
 	static int lives = 10;
 
 	static JLabel label = new JLabel();
+	static JFrame frame = new JFrame();
+	static JPanel panel = new JPanel();
+	Stack<String> words = new Stack<String>();
 
 	public static void main(String[] args) {
 		HangMan hangman = new HangMan();
+		hangman.setup();
 		hangman.accesories();
 
 	}
 
 	public void accesories() {
-		Stack<String> words = new Stack<String>();
-
+		
 		String input = JOptionPane.showInputDialog("How many words would you like to guess up to 266??");
 		int inputt = Integer.parseInt(input);
-
+		lives = 10;
+		
+		
 		for (int i = 0; i < inputt; i++) {
 			String dictionary = Utilities.readRandomLineFromFile("dictionary.txt");
 			if (!words.contains(dictionary)) {
@@ -35,29 +40,30 @@ public class HangMan implements KeyListener {
 			}
 
 		}
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
-		frame.setVisible(true);
-		frame.setSize(500, 50);
-		panel.add(label);
-		frame.addKeyListener(this);
-		frame.add(panel);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 
 		word = words.pop();
-
-		label.setText(line + lives);
-		if (!words.contains("_")) {
-			words.pop();
+			
 			int length = word.length();
 			System.out.println(word);
 			line = "";
 			for (int i = 0; i < length; i++) {
 				line += "_";
 			}
-		}
+		
+		label.setText(line + lives);
 	}
 
+	public void setup() {
+		
+		frame.setVisible(true);
+		frame.setSize(500, 50);
+		panel.add(label);
+		frame.addKeyListener(this);
+		frame.add(panel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
@@ -67,6 +73,7 @@ public class HangMan implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
+
 		if (word.contains(e.getKeyChar() + "")) {
 			String guess = e.getKeyChar() + "";
 
@@ -76,11 +83,36 @@ public class HangMan implements KeyListener {
 				}
 			}
 
-		} else {
-			lives--;
+		} else if (!word.contains(e.getKeyChar() + "")) {
+			lives = lives-1;
+		
+		} 
+			
 
+		
+		if (lives <= 0) {
+		int yesno =	JOptionPane.showConfirmDialog(null, "You lose. Would you like to try again?","Game over! :P", JOptionPane.YES_NO_OPTION);
+		
+		if (yesno == 0) {
+			accesories();
+			
 		}
-		label.setText(line + lives);
+		else if (yesno == 1) {
+			System.exit(yesno);
+		}
+		}
+		if (!line.contains("_")) {
+
+			word = words.pop();
+			
+			int length = word.length();
+			System.out.println(word);
+			line = "";
+			for (int i = 0; i < length; i++) {
+				line += "_";
+			}
+		}
+		label.setText(line + lives); 
 	}
 
 	@Override
--- src/_05_String_Integer_Pair/StringIntegerPair.java
+++ src/_05_String_Integer_Pair/StringIntegerPair.java
@@ -18,50 +18,73 @@ public class StringIntegerPair {
 		//   the passed in String, set the value at that location to the
 		//   passed in value and return from the method.
 		for (int i = 0; i < keys.length; i++) {
-			if (keys.equals(values[i])) {
-				
+			if (keys[i].equals(key)) {
+				 values[i]= value;
 			}
 		}
 		//B. create a String array that is one element longer than the keys
-		
+		String[] keys1 = new String[keys.length+1];
 		//C. create an integer array that is one element longer than values
-		
+		int[] values1 = new int[values.length+1];
 		//D. set the last element of the new String array to the passed in key
-		
+		keys1[keys1.length-1] = key;
 		//E. set the last element of the new int array to the passed in value
-		
+		values1[values1.length-1] = value;
 		//F. iterate through the keys and values and copy the elements to the new arrays
-		
+		for (int i = 0; i < values.length; i++) {
+			values1[i] = values[i];
+			
+		}
+		for (int i = 0; i < keys.length; i++) {
+			keys1[i] = keys[i];
+		}
+		keys = keys1;
+		values = values1;
 		//G. Set the keys and values arrays equal to the new arrays created in steps B and C.
 	}
 	
 	//5. Complete the method so it returns the value located at the passed in key.
 	//   If the key does not exist, return Integer.MIN_VALUE.
 	public int get(String key) {
-		return 0;
+		for (int i = 0; i < keys.length; i++) {
+			if (key.equals(keys[i])) {
+			return	values[i];
+			}
+		
+		}
+		return Integer.MIN_VALUE;
 	}
 	
 	//6. Complete the containsKey method so that it returns true if the
 	//   passed in keys is contained in the keys array
 	public boolean containsKey(String key) {
-		
+		for (int i = 0; i < keys.length; i++) {
+			if (key.equals(keys[i])) {
+				return true;
+			}
+		}
 		return false;
 	}
 	
 	//7. Complete the containsValue method so that it returns true if the
 	//   passed in value is contained in the values array
 	public boolean containsValue(int value) {
-		
+		for (int i = 0; i < values.length; i++) {
+			if (value == values[i]) {
+				return true;
+			}
+		}
 		return false;
 	}
 	
 	//8. Complete the getKeysMethod so it returns the keys as an array
 	public String[] getKeys() {
-		return null;
+	
+		return keys;
 	}
 	
 	//9. Complete the getValues so it returns the values as an array
 	public int[] getValues() {
-		return null;
+		return values;
 	}
 }
